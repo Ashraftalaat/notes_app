370f81197d8bbec061dae0981b64f7182a7645b3
Ashraftalaat <atslook@gmail.com>
2023-07-29 00:38:01 +0300

    create type adabter for note

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,19 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+//اسم الفيل اللي هيتعمل له generate بلغة dart
+part 'note_model.g.dart';
+
+//يمكن في الكلاس تكرار الرقم
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+//لا يمكن تكرار الرقم
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
@@ -10,3 +22,5 @@ class NoteModel {
       required this.date,
       required this.color});
 }
+//بعد كدة هنفتح New terminal ونضع الامر دة
+//flutter packages pub run build_runner build
