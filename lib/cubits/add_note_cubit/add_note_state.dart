1bf7ccbe99592adb20df94c76b5cdd23d7d57d26
Your NameAshraftalaat <atslook@gmail.com>
2023-08-01 23:42:59 +0300

    add add notes state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,3 +4,18 @@ part of 'add_note_cubit.dart';
 abstract class AddNoteState {}
 
 class AddNoteInitial extends AddNoteState {}
+
+//هناك في الاغلب ثوابت في state
+//1//Loading عشان هيبقي في future
+class AddNoteLoading extends AddNoteState {}
+
+//2/Success عشان هيبقي فية عملية ناجحة
+class AddNoteSuccess extends AddNoteState {}
+
+//3/Failure عشان هيبقي فية عملية فاشلة
+class AddNoteFailure extends AddNoteState {
+  //هيكون فية رسالة الخطاء
+  final String errMessage;
+
+  AddNoteFailure(this.errMessage);
+}
